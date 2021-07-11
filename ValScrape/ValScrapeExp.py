092dc57dff6cd60805fc1fc2ff756f331b4c13cc
Alycia Wong <alyciacwong@gmail.com>
2021-07-10 20:17:20 -0400

    Shorten and Comment on Performance Scraper
    added descriptions, to do's, and for loops to scraper.
    fixed bugs that collected information from all games table.

--- ValScrape/ValScrapeExp.py
+++ ValScrape/ValScrapeExp.py
@@ -1,36 +1,30 @@
 import pandas as pd
 
-# link = input("URL? \n")   # Set vrl.gg performance URL as variable "link"
-# url = r link              # Read in link
-url = r'https://www.vlr.gg/25193/version1-vs-100-thieves-champions-tour-north-america-stage-3-challengers-1-ubqf/?game=all&tab=performance'
+# TO DO: Create "performance scraper" class -- subclass to "scraper" class
+# TO DO: Figure out how to read in modular URL
+# link = 'vrl.gg/' + input("Match code? \n") + '/?game=all&tab=performance' # Get "link" for scraping
+# url = r link  # Read in link
+url = r'https://www.vlr.gg/25193/?game=41573&tab=performance'    # placeholder
 
-match = pd.read_html(url)   # Total number of tables in static page
-games = len(ball)/4-1       # Total number of games
-tables =[7, 11, 15]         # Tables of games 1, 2, and/or 3
+match = pd.read_html(url)               # Total number of tables in static page
+games = int(len(match)/4-1)             # Total number of games
+tables = list(range(games))             # Placeholders for tables
+# Check if there are any games for given match code
+if games == 0:
+    # TO DO: table filled with zeros
+    print("No Matches Up")
+# Make tables list hold table index for each map
+for i in range(games):
+    tables[i] = 4 * (i+1) + 3
 
-#
-if games == 2:
-    tables = tables[0:2]
-elif games == 1:
-    tables = tables[0]
-elif games == 0:
-    # table filled with zeros
-    tables = tables
-
-for i in tables:
-    print(i)
-    # creates a different dataframe, to put all the data frames into a tuple
-    df = pd.read_html(url)[i]
-    df.fillna("0 d", inplace=True)
-    df.rename(columns={'Unnamed: 0':'Player'}, inplace=True)
-    df[['Player', 'Team']] = df['Player'].str.split(n=0, expand=True)
-    df[['2K', 'd0']] = df['2K'].str.split(n=1, expand=True)
-    df[['3K', 'd1']] = df['3K'].str.split(n=1, expand=True)
-    df[['4K', 'd2']] = df['4K'].str.split(n=1, expand=True)
-    df[['5K', 'd3']] = df['5K'].str.split(n=1, expand=True)
-    df[['1v1', 'd4']] = df['1v1'].str.split(n=1, expand=True)
-    df[['1v2', 'd5']] = df['1v2'].str.split(n=1, expand=True)
-    df[['1v3', 'd6']] = df['1v3'].str.split(n=1, expand=True)
-    df[['1v4', 'd7']] = df['1v4'].str.split(n=1, expand=True)
-    df[['1v5', 'd8']] = df['1v5'].str.split(n=1, expand=True)
-    df = df[['Player', '2K', '3K', '4K', '5K', '1v1', '1v2', '1v3', '1v4', '1v5']]
+cols = ['2K', '3K', '4K', '5K', '1v1', '1v2', '1v3', '1v4', '1v5']  # Statistic titles
+# Parse, clean, and organize dataframes
+for j in range(games):
+    df = pd.read_html(url)[tables[j]]                                   # Parse table
+    df.fillna("0 0", inplace=True)                                      # Replace all NaN values with strings
+    df.rename(columns={'Unnamed: 0':'Player'}, inplace=True)            # Name Player column
+    df[['Player', 'Team']] = df['Player'].str.split(n=1, expand=True)   # Split Player column into Player and Team
+    for k in range(len(cols)):
+        df[[cols[k], 'd'+str(k)]] = df[cols[k]].str.split(n=1, expand=True)     # Dump extraneous information
+    df = df[['Player'] + cols]                                                  # Re-organize dataframe
+    tables[j] = df                                                              # Add dataframe to list
