6350df7786a90a5a56c46ebab0df2b6539a62939
Alycia Wong <alyciacwong@gmail.com>
2021-07-11 22:10:36 -0400

    Fix Warnings
    Got rid of shadow names, added white spaces, changed Class_Names to CamelCase, shortened descriptions, added instances, and corrected spelling errors.
    Added a ParseTable class to parse through cleaned data.
    Error in url opening updated to line 17.

--- fantasyVCT/PerformanceExp.py
+++ fantasyVCT/PerformanceExp.py
@@ -1,62 +1,79 @@
 import pandas as pd
 
-class Scraper():
 
-    def scrape_url(self, match_code, game_type):
+class Scraper:
+    # TODO: accept match code. Right now from outer scope
+    def __init__(self, match_code):
+        self.match_code = match_code
+        self.url = 'string'
+        self.matches = ''
+        self.df = ''
+
+    def scrape_url(self):
         print('2')
-        url = 'https://www.vrl.gg/' + match_code + '/?game=all&tab=' + game_type  # Get "link" for scraping
+        self.url = 'https://www.vrl.gg/' + match_code + '/?game=all&tab=' + self.game_type  # Get "link" for scraping
         print('2.5')
-        # ERROR IN LINE 10 "<urlopen error [WinError 10061] No connection could be made because the target machine actively refused it>"
-        matches = pd.read_html(url)  # Compilation of all tables in static page
+        # ERROR IN LINE 17 "<urlopen error [WinError 10061] No connection could be made because the target machine actively refused it>"
+        self.matches = pd.read_html(self.url)  # Compilation of all tables in static page
         print("3")
-        return url, matches
 
-    def clean_data(self, df, cols):
+    def clean_data(self):
         print('9')
-        df.fillna("0 0", inplace=True)  # Replace all NaN values with strings
-        df.rename(columns={'Unnamed: 0': 'Player'}, inplace=True)  # Name Player column
-        df[['Player', 'Team']] = df['Player'].str.split(n=1, expand=True)  # Split Player column into Player and Team
+        self.df.fillna("0 0", inplace=True)  # Replace all NaN values with strings
+        self.df.rename(columns={'Unnamed: 0': 'Player'}, inplace=True)          # Name Player column
+        self.df[['Player', 'Team']] = df['Player'].str.split(n=1, expand=True)  # Split Player column into Player/Team
         print('10')
-        for k in range(len(cols)):
-            df[[cols[k], 'd' + str(k)]] = df[cols[k]].str.split(n=1, expand=True)  # Dump extraneous information
+        # Dump extraneous information
+        for k in range(len(self.cols)):
+            self.df[[self.cols[k], 'd' + str(k)]] = self.df[self.cols[k]].str.split(n=1, expand=True)
             print('11')
-        return df[['Player'] + cols]
+        # Reorganize table
+        self.df = self.df[['Player'] + self.cols]
 
-    def organize_data(self, url, games, tables, cols):
+    def organize_data(self):
         print('8')
-        for j in range(games):
-            df = pd.read_html(url)[tables[j]]  # Parse table
-            self.clean_data(df, cols)
-            tables[j] = df  # Add dataframe to list
+        for j in range(self.games):
+            self.df = pd.read_html(self.url)[self.tables[j]]  # Parse table
+            self.clean_data()
+            self.tables[j] = df  # Add dataframe to list
         print('12')
-        return tables
 
 
-class Performance_Scraper(Scraper):
+class PerformanceScraper(Scraper):
 
-    def which_games(self, matches):
+    def __init__(self):
+        super().__init__(match_code)
+        self.game_type = 'performance'
+        self.cols = ['2K', '3K', '4K', '5K', '1v1', '1v2', '1v3', '1v4', '1v5']  # Statistic titles
+        self.games = int(len(self.matches) / 4 - 1)     # Total number of games
+        self.tables = list(range(self.games))           # Placeholders for tables
+
+    def which_games(self):
         print('5')
-        cols = ['2K', '3K', '4K', '5K', '1v1', '1v2', '1v3', '1v4', '1v5']  # Statistic titles
-        games = int(len(matches) / 4 - 1)  # Total number of games
-        tables = list(range(games))  # Placeholders for tables
         # Check if there are any games for given match code
-        if games == 0:
+        if self.games == 0:
             # TO DO: table filled with zeros
             print("No Matches Up")
         # Make tables list hold table index for each map
         for i in range(games):
-            tables[i] = 4 * (i + 1) + 3
+            self.tables[i] = 4 * (i + 1) + 3
         print('6')
-        return cols, games, tables
 
-    def final_tables(self, match_code):
-        game_type = 'performance'
+    def final_tables(self):
         print("1")
-        url, matches = Scraper.scrape_url(self, match_code, game_type)
+        Scraper.scrape_url(self)
         print('4')
-        cols, games, tables = self.which_games(matches)
+        self.which_games()
         print('7')
-        return Scraper.organize_data(url, games, tables, cols)
+        Scraper.organize_data(self)
+
+
+# Will parse through tables and extract/organize information
+class ParseTable:
+    def __init__(self):
+        self.player = ''
+        self.team = ''
+        self.match = ''
 
 
 match_code = '41573'
