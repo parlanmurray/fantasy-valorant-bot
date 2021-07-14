571340a18101d7570ee66ecff74b5930db10048f
Parlan Murray <parlanmurray@gmail.com>
2021-07-14 18:55:35 -0400

    Pytest
    Configure test file to use pytest. Ignore pytest generated files.

--- test/test_scraper.py
+++ test/test_scraper.py
@@ -6,17 +6,9 @@ scraper = Scraper()
 maps = scraper.parse_match(25206)
 
 def test_map_names():
-	assert len(maps) == 3
+	assert len(maps) == 2
 	assert maps[0].name == "Haven"
 	assert maps[1].name == "Breeze"
-	assert maps[2].name == "Ascent"
-
-def test_map_played():
-	assert maps[0].was_played == True
-	assert maps[2].was_played == False
-
-def test_map_number():
-	assert maps[0].number is 1
 
 def test_team_name():
 	assert maps[0].team1.name == "100 Thieves"
