713931fc393e3066988ae9b45b9b6c695c74aebb
Parlan Murray <parlanmurray@gmail.com>
2021-07-14 21:49:43 -0400

    Pretty print
    Implement __str__ for pretty printing of stored information.

--- fantasyVCT/valorant.py
+++ fantasyVCT/valorant.py
@@ -1,4 +1,6 @@
 
+# TODO pretty print
+
 class Player:
 	def __init__(self, name: str, agent: str):
 		self.name = name
@@ -18,6 +20,29 @@ class Player:
 		'1v5': 0
 		}
 
+	def __str__(self):
+		format_str = ""
+		format_str += "{}\t\t{}\t{}\t{}/{}/{}\t".format(
+			self.name, 
+			self.agent, 
+			self.stats['acs'], 
+			self.stats['kills'], 
+			self.stats['deaths'],
+			self.stats['assists']
+			)
+		if self.stats['kills'] < 10 or self.stats['deaths'] < 10 or self.stats['assists'] < 10:
+			format_str += "\t"
+		format_str +="{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
+			self.stats['2k'],
+			self.stats['3k'],
+			self.stats['4k'],
+			self.stats['5k'],
+			self.stats['1v2'],
+			self.stats['1v3'],
+			self.stats['1v4'],
+			self.stats['1v5'])
+		return format_str
+
 	def set_stat_int(self, key, value):
 		self.stats[key] = int(value)
 
@@ -29,6 +54,27 @@ class Team:
 		self.won = won
 		self.score = score
 		self.map_pick = False
+
+	def __str__(self):
+		format_str = ""
+		format_str += "{0}\t{1}".format(
+			self.name,
+			self.score
+		)
+		if self.won:
+			format_str += " -- Winner"
+		if self.map_pick:
+			format_str += " -- Map Pick"
+		format_str += "\nPlayer\t\tAgent\tACS\tK/D/A\t\t2k\t3k\t4k\t5k\t1v2\t1v3\t1v4\t1v5"
+		format_str += "\n{0}\n{1}\n{2}\n{3}\n{4}\n".format(
+			self.players[0],
+			self.players[1],
+			self.players[2],
+			self.players[3],
+			self.players[4]
+		)
+		return format_str
+
 
 	def add_player(self, player: Player):
 		self.players.append(player)
@@ -43,6 +89,14 @@ class Map:
 		self.game_id = game_id
 		self.team1 = None
 		self.team2 = None
+	
+	def __str__(self):
+		return "{}\tGame ID: {}\n\n{}\n{}".format(
+			self.name, 
+			self.game_id, 
+			self.team1, 
+			self.team2
+		)
 
 	def set_team(self, team: Team):
 		if not self.team1:
