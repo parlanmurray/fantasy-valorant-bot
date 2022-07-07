2a069731d33bfed27412666900e614397890f4cb
Parlan Murray <parlanmurray@gmail.com>
2022-07-07 17:29:36 -0400

    Update !help
    Make the help command more informative. Refactor !upload command to a
    different cog, and (hopefully) have better category names.

--- fantasyVCT/vlr_api.py
+++ fantasyVCT/vlr_api.py
@@ -7,7 +7,7 @@ from discord.ext import tasks, commands
 
 vlr_api = "https://vlrggapi.herokuapp.com/{}"
 
-class FetchCog(commands.Cog):
+class FetchCog(commands.Cog, name="Results"):
 	def __init__(self, bot):
 		self.bot = bot
 		self.get_results.start()
@@ -17,6 +17,7 @@ class FetchCog(commands.Cog):
 
 	@commands.command()
 	async def update(self, ctx):
+		"""Get new match results early"""
 		r = requests.get(vlr_api.format("match/results"))
 		json = r.json()
 
@@ -47,6 +48,52 @@ class FetchCog(commands.Cog):
 					continue
 
 				await ctx.invoke(self.bot.get_command('upload'), vlr_id)
+
+	@commands.command()
+	async def upload(self, ctx, vlr_id: str):
+		"""Upload a vlr.gg match"""
+		# check that the input is valid
+		if not re.match("^[0-9]{5,6}$", vlr_id):
+			return await ctx.send("Not a valid vlr match number.")
+
+		# check that match does not exist in database
+		if self.bot.db_manager.query_results_all_from_match_id(vlr_id):
+			return await ctx.send("This match has already been uploaded.")
+		
+		# parse link
+		results = self.bot.scraper.parse_match(vlr_id)
+
+		# verify teams and players exist in database
+		for _map in results.maps:
+			for team in (_map.team1, _map.team2):
+				# check that teams exist in database
+				team_info = self.bot.db_manager.query_team_all_from_name(team.name)
+				if not team_info:
+					# team does not exist in database
+					self.bot.db_manager.insert_team_to_teams(team.name, team.abbrev, "TEST")
+					self.bot.db_manager.commit()
+					team_info = self.bot.db_manager.query_team_all_from_name(team.name)
+
+				team_id = team_info[0]
+
+				for player in team.players:
+					# check that players exist in database
+					player_info = self.bot.db_manager.query_players_all_from_name(player.name)
+					if not player_info:
+						# player does not exist in database
+						self.bot.db_manager.insert_player_to_players(player.name, team_id)
+						player_info = self.bot.db_manager.query_players_all_from_name(player.name)
+					elif not player_info[2] or player_info[2] != team_id:
+						# player is not assigned to a team
+						self.bot.db_manager.update_players_team_id(player_info[0], team_id)
+
+					# upload data
+					self.bot.db_manager.insert_result_to_results(_map.name, _map.game_id, vlr_id, player_info[0], player, None)
+					self.bot.db_manager.commit()
+
+		self.bot.cache.invalidate()
+		for _map in results.maps:
+			await ctx.send("```\n" + str(_map) + "\n```")
 
 	@tasks.loop(hours=1.0)
 	async def get_results(self):
